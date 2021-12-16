4f0680e9d9d11812e74b052d41da45adebd585f9
Konstantin Popovskikh <remakerich@mail.ru>
2021-12-16 16:39:51 +0600

    initial

--- lib/bloc/weather_repo.dart
+++ lib/bloc/weather_repo.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:vosmerka_flutter_weather/models/city.dart';
 
@@ -13,7 +16,8 @@ class WeatherRepo {
   ];
 
   Future<City> getWeather(String city) async {
-    const _apiKey = 'a3d77f43f166bfd1905b1c53e6f92aaf';
+    final _apiKeyFromAsset = await rootBundle.loadString('assets/api_key.json');
+    final _apiKey = jsonDecode(_apiKeyFromAsset)['key'];
     final _endpoint =
         'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$_apiKey&units=metric';
     final result = await http.Client().get(Uri.parse(_endpoint));

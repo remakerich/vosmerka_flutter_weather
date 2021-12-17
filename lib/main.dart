16dadc7e8e4ad3432c367d9184a5abbd06f20771
Konstantin Popovskikh <remakerich@mail.ru>
2021-12-17 21:50:20 +0600

    apply theme to descendant widgets

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Theme.of(context);
     getApiKey();
     return BlocProvider(
       create: (context) => WeatherBloc(),
